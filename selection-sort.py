47303e3b225108854cd5a9d40791e627d198f7be
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-10-05 15:07:17 -0700

    Update selection-sort.py

--- selection-sort.py
+++ selection-sort.py
@@ -1,11 +1,15 @@
+class Solution: 
+    def select(self, arr, i):
+        # code here
+        idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[idx]:
+                idx = j
+        return idx
+    
     def selectionSort(self, arr,n):
         #code here
-        
         for i in range(n):
-            idx = i
-            
-            for j in range(i + 1, n):
-                if arr[j] < arr[idx]:
-                    idx = j
-            
+            idx = self.select(arr, i)
             arr[i], arr[idx] = arr[idx], arr[i]
+        return arr

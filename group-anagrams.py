a19f5d6293cf0d2a5c61555b9dcc2216b94feab8
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-11-08 01:42:04 -0800

    Update group-anagrams.py

--- group-anagrams.py
+++ group-anagrams.py
@@ -1,13 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strsMap = {}
+        strsMap = defaultdict(list)
 
         for i in strs:
             sortI = "".join(sorted(i))
-
-            if sortI not in strsMap:
-                strsMap[sortI] = []
-
             strsMap[sortI].append(i)
 
-        return list(strsMap.values())
+        return strsMap.values()

54b2882fb0a3dcd3a67b2f8f9a7b4557b8370d61
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-11-09 08:37:38 -0800

    Update product-of-array-except-self.py

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,26 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prifixMul = [nums[0]]
-        prifixMulRe = [nums[-1]]
-        res = []
+        res = [1] * len(nums)
 
-        for i in range(1, len(nums)):
-            mul = prifixMul[-1] * nums[i]
-            prifixMul.append(mul)
-
-        for i in range(len(nums) - 2, -1, -1):
-            mul = prifixMulRe[-1] * nums[i]
-            prifixMulRe.append(mul)
-
-        prifixMulRe.reverse()
-
+        prifix = 1
         for i in range(len(nums)):
-            if i == 0:
-                res.append(prifixMulRe[1])
-            elif i == len(nums) - 1:
-                res.append(prifixMul[len(nums) - 2])
-            else:
-                ans = prifixMul[i - 1] * prifixMulRe[i + 1]
-                res.append(ans)
+            res[i] = prifix
+            prifix *= nums[i]
+
+        suffix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
 
         return res

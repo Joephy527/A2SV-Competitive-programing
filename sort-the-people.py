bdf7e683cec5daffe27ca762e1b42a65b6431f6d
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-11-21 05:28:52 -0800

    Update sort-the-people.py

--- sort-the-people.py
+++ sort-the-people.py
@@ -1,10 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names) - 1):
-            idx = i
+        def select(index):
+            idx = index
             for j in range(i + 1, len(names)):
                 if heights[idx] < heights[j]:
                     idx = j
+
+            return idx
+        
+        for i in range(len(names) - 1):
+            idx = select(i)
 
             heights[i], heights[idx] = heights[idx], heights[i]
             names[i], names[idx] = names[idx], names[i]

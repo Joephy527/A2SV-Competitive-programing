c925fb7ac777d046a52a921b0a40f2189a873b77
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-11-22 14:46:14 -0800

    Update divide-players-into-teams-of-equal-skill.py

--- divide-players-into-teams-of-equal-skill.py
+++ divide-players-into-teams-of-equal-skill.py
@@ -1,7 +1,5 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        if len(skill) == 2: return skill[0] * skill[1]
-
         skill.sort()
         l, r = 0, len(skill) - 1
         Sum = skill[l] + skill[r]
@@ -10,9 +8,8 @@ class Solution:
         while l < r:
             if skill[l] + skill[r] != Sum:
                 return -1
-            else:
-                res += skill[l] * skill[r]
-
+            
+            res += skill[l] * skill[r]
             l += 1
             r -= 1
 

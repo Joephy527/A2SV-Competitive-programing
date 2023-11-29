e6616eb7c3c8232018ab8019ec56b2a4ec6d9906
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-11-29 03:16:10 -0800

    Update car-pooling.py

--- car-pooling.py
+++ car-pooling.py
@@ -1,19 +1,16 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key=lambda trip: trip[1])
-        dropOff = set()
+        passangers = [0] * (max(trip[2] for trip in trips) + 1)
+
+        for trip in trips:
+            passangers[trip[1]] += trip[0]
+            passangers[trip[2]] -= trip[0]
+
         s = 0
 
-        for i in range(len(trips)):
-            s += trips[i][0]
-            j = i - 1
-            
-            while j >= 0:
-                if trips[j][2] <= trips[i][1] and j not in dropOff:
-                    s -= trips[j][0]
-                    dropOff.add(j)
-                j -= 1
-
-            if s > capacity: return False
-
+        for passanger in passangers:
+            s += passanger
+            if s > capacity:
+                return False
+        
         return True

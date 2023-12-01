4efe5165976fbe57ac172c56d62ebae60df8e07a
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-12-01 04:02:08 -0800

    Update relative-sort-array.py

--- relative-sort-array.py
+++ relative-sort-array.py
@@ -1,20 +1,24 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        dic = {}
-        nums, ans = [], []
-        
-        for val in arr2:
-            dic[val] = 0
-        
-        for val in arr1:
-            if val not in dic:
-                nums.append(val)
-            else: 
-                dic[val] += 1
-                
-        for key, val in dic.items():
-            ans.extend([key for _ in range(val)])
-        
-        ans.extend(sorted(nums))
-        
+        count = {}
+        rest, ans = [], []
+
+        for num in arr2:
+            count[num] = 0
+
+        for num in arr1:
+            if num in count:
+                count[num] += 1
+            else:
+                rest.append(num)
+
+        for val, amt in count.items():
+            while amt:
+                ans.append(val)
+                amt -= 1
+
+        rest.sort()
+        for num in rest:
+            ans.append(num)
+
         return ans

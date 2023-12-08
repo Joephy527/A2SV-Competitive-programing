964bdef661c81ad10a18690ab2c54d6586f93ec1
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2023-12-08 07:36:06 -0800

    Update shifting-letters-ii.py

--- shifting-letters-ii.py
+++ shifting-letters-ii.py
@@ -1,18 +1,18 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        n = len(s)
-        d = [0 for _ in range(n + 1)]
-        
-        for shift in shifts:
-            di = 1 if shift[2] else -1
-            d[shift[0]] += di
-            d[shift[1] + 1] -= di
-        
-        res = []
-        for i in range(n):
-            if i != 0: 
-                d[i] += d[i - 1]
-            
-            newChr = (ord(s[i]) - ord("a") + d[i]) % 26 + ord("a")
-            res.append(chr(newChr))
-        return "".join(res)
+        amountToShift = [0] * (len(s) + 1)
+        shifted = 0
+        ans = ""
+
+        for start, end, direction in shifts:
+            newDir = 1 if direction == 1 else -1
+            amountToShift[start] += newDir
+            amountToShift[end + 1] -= newDir
+
+        for i in range(len(s)):
+            shifted += amountToShift[i]
+            amountShifted = (ord(s[i]) - ord("a") + shifted) % 26
+            newChar = amountShifted + ord("a")
+            ans += chr(newChar)
+
+        return ans

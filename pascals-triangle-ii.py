8198d77ba86810087777d058bb2401aecc5a4f5d
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-01-04 12:05:46 -0800

    Create pascals-triangle-ii.py

---
+++ pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0: return [1]
+        if rowIndex == 0: return [1, 1]
+
+        arr = self.getRow(rowIndex - 1)
+        res = [1] * (rowIndex + 1)
+        
+        for i in range(1, rowIndex):
+            res[i] = arr[i - 1] + arr[i]
+
+        return res

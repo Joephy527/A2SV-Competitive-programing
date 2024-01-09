14d9be73dcea481f7b128863d48419e7d6d2dee9
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-01-09 10:46:35 -0800

    Update linked-list-cycle.py

--- linked-list-cycle.py
+++ linked-list-cycle.py
@@ -6,12 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hashSet = set()
+        slow = fast = head
 
-        while head:
-            hashSet.add(head)
-            
-            if head.next in hashSet:
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
-
-            head = head.next

36203182bbc39799221728c84638fb4e787a8c47
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-01-16 05:46:58 -0800

    Update Fizz-buzz.py

--- Fizz-buzz.py
+++ Fizz-buzz.py
@@ -1,13 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        answer = []
+        dic = {3: "Fizz", 5: "Buzz"}
+        res = []
+        
         for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append('FizzBuzz')
-            elif i % 3 == 0:
-                answer.append('Fizz')
-            elif i % 5 == 0:
-                answer.append('Buzz')
-            else:
-                answer.append(str(i))
-        return answer
+            phrase = ""
+            for num, word in dic.items():
+                if i % num == 0:
+                    phrase += word
+
+            res.append(phrase if phrase != "" else str(i))
+
+        return res

a8d35b8953bdeae496c76a1f92eb1c86840cf693
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-02-05 12:03:01 -0800

    Create nested-list.py

---
+++ nested-list.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    records = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        record = [name, score]
+        records.append(record)
+        
+    records.sort(key=lambda rec: (rec[1], rec[0]))
+    minScore = records[0][1]
+    secondMinScore = records[-1][1]
+    
+    for name, score in records:
+        if score != minScore:
+            secondMinScore = min(secondMinScore, score)
+            
+    for name, score in records:
+        if score == secondMinScore:
+            print(name)

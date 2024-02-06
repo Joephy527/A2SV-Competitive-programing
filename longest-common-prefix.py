1589a9e8e33a2bd6a323cbc1d7bd0a7c7dd8e2d3
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-02-06 10:56:51 -0800

    Update longest-common-prefix.py

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -1,15 +1,39 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        answer = ""
-        strs.sort()
-
-        # get shortest word from the first and last word
-        smallestWordLength = min(len(strs[0]),len(strs[-1]))
+        trie = Trie()
+        for word in strs:
+            trie.insert(word)
         
-        for i in range(smallestWordLength):
-            if strs[0][i] != strs[-1][i]:
-                return answer
-            
-            answer += strs[0][i]
+        return trie.longestPrefix()
 
-        return answer
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+    
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        cur = self.root
+
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+
+            cur = cur.children[char]
+
+        cur.isWord = True
+            
+    def longestPrefix(self):
+        cur = self.root
+        longest = ""
+
+        while cur and len(cur.children) == 1 and not cur.isWord:
+            longest += list(cur.children)[0]
+            cur = cur.children[list(cur.children)[0]]
+        
+        return longest

76092bff60f24a2ea887d715f74ba7eab213a31c
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-02-07 03:07:06 -0800

    Update longest-word-in-dictionary.py

--- longest-word-in-dictionary.py
+++ longest-word-in-dictionary.py
@@ -1,47 +1,43 @@
 class Solution:
     def longestWord(self, words: List[str]) -> str:
+        words.sort()
         trie = Trie()
         longestWord = ""
 
         for word in words:
-            trie.insert(word)
+            lastNode = trie.insert(word)
 
-        for word in words:
-            if trie.checkPrefixes(word):
-                if len(longestWord) == len(word):
-                    longestWord = min(longestWord, word)
-                elif len(longestWord) < len(word):
-                    longestWord = word
+            if (lastNode.val == len(word) and
+                lastNode.val > len(longestWord)):
+                longestWord = word
 
         return longestWord
+
 
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode(0)
 
     def insert(self, word: str) -> None:
         cur = self.root
+        count = 0
         
         for char in word:
             if char not in cur.children:
-                cur.children[char] = TrieNode()
+                cur.children[char] = TrieNode(count)
             
             cur = cur.children[char]
 
+            if cur.isEnd: count += 1
+
         cur.isEnd = True
+        cur.val += 1
 
-    def checkPrefixes(self, word: str) -> bool:
-        cur = self.root
-
-        for char in word:
-            cur = cur.children[char]
-            if not cur.isEnd:
-                return False
-
-        return True
+        return cur
 
 class TrieNode():
-    def __init__(self):
+    def __init__(self, val):
         self.isEnd = False
         self.children = {}
+        self.val = val

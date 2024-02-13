10de0503fbaab497dc1e920a4a7c3ec73edef7c5
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-02-13 10:15:00 -0800

    Update majority-element.py

--- majority-element.py
+++ majority-element.py
@@ -1,4 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a = [i for i, j in Counter(nums).most_common()]
-        return a[0]
+        countMap = Counter(nums)
+        majority = countEle = 0
+
+        for num in countMap:
+            if countEle < countMap[num]:
+                countEle = countMap[num]
+                majority = num
+
+        return majority

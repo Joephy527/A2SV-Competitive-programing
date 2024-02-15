57e9bae5fa79a74c42b4a6aa42f1c748fea91d0c
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-02-15 11:32:37 -0800

    Update koko-eating-bananas.py

--- koko-eating-bananas.py
+++ koko-eating-bananas.py
@@ -1,19 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
-        ans = r
+        minBanana = r
 
         while l <= r:
-            m = (l + r) // 2
-            totalTime = 0
+            k = (l + r) // 2
+            time = 0
 
             for pile in piles:
-                totalTime += math.ceil(float(pile) / m)
-            
-            if totalTime <= h:
-                ans = m
-                r = m - 1
-            else:
-                l = m + 1
+                time += ceil(pile / k)
 
-        return ans
+            if time <= h:
+                minBanana = k
+                r = k - 1
+            else:
+                l = k + 1
+
+        return minBanana

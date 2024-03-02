15af262d5606ec1f87bf15c9486eea98dfcaae6a
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-03-02 01:41:10 -0800

    Update pancake-sorting.py

--- pancake-sorting.py
+++ pancake-sorting.py
@@ -1,12 +1,12 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        def swap(arr, l, r):
+        def swap(r, l = 0):
             while l < r:
                 arr[l], arr[r] = arr[r], arr[l]
                 l += 1
                 r -= 1
 
-        def select(arr, index):
+        def select(index):
             idx = index
             for i in range(index):
                 if arr[i] > arr[idx]:
@@ -17,14 +17,15 @@ class Solution:
         res = []
 
         for i in range(len(arr) - 1, -1, -1):
-            idx = select(arr, i)
+            idx = select(i)
 
             if idx == i: continue
 
             if idx != 0:
-                swap(arr, 0, idx)
+                swap(idx)
                 res.append(idx + 1)
-            swap(arr, 0, i)
+            
+            swap(i)
             res.append(i + 1)
 
         return res

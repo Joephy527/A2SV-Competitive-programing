63835ca27f85e6c80080735531bbe2a6db0c104f
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-04-24 20:39:26 +0300

    Update next-greater-element-i.py

--- next-greater-element-i.py
+++ next-greater-element-i.py
@@ -1,16 +1,19 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        dic = {num: i for i, num in enumerate(nums2)}
-        res = []
+        stack = []
+        nextGreater = {}
+        greater = []
+
+        for num in nums2:
+            while stack and stack[-1] < num:
+                nextGreater[stack.pop()] = num
+
+            stack.append(num)
+
+        while stack:
+            nextGreater[stack.pop()] = -1
 
         for num in nums1:
-            idx = dic[num]
-            greater = -1
-            for i in range(idx + 1, len(nums2)):
-                if nums2[i] > num:
-                    greater = nums2[i]
-                    break
+            greater.append(nextGreater[num])
 
-            res.append(greater)
-
-        return res
+        return greater

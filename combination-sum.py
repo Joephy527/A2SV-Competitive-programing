69d0400478746a92dd108cb85f94b9bbfce4eae8
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-06-10 12:29:20 +0300

    Update combination-sum.py

--- combination-sum.py
+++ combination-sum.py
@@ -1,23 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        comb, sub = [], []
+
         def backTrack(idx = 0, total = 0):
-            if idx >= len(candidates) or total > target: 
+            if total >= target or idx == len(candidates):
+                if total == target:
+                    comb.append(sub[:])
+
                 return
 
-            if total == target:
-                subSets.append(subSet[:])
-                
-                return
-
-            subSet.append(candidates[idx])
+            sub.append(candidates[idx])
             backTrack(idx, total + candidates[idx])
-
-            subSet.pop()
+            
+            sub.pop()
             backTrack(idx + 1, total)
-
-        subSets = []
-        subSet = []
 
         backTrack()
 
-        return subSets
+        return comb

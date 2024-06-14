8822e446769ff6a070e94e5fb43b0c8712970b04
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-06-14 22:58:35 +0300

    Update additive-number.py

--- additive-number.py
+++ additive-number.py
@@ -1,34 +1,25 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        def backTrack(idx, first, second):
-            if idx == len(num):
+        sub = []
+        
+        def backTrack(idx):
+            if len(sub) > 2 and not sub[-1] == sub[-2] + sub[-3]:
+                return
+
+            if idx == len(num) and len(sub) > 2:
                 return True
-
-            if second or second == 0:
-                for i in range(idx, len(num)):
-                    newCur = num[idx:i + 1]
-
-                    if len(newCur) > 1 and newCur[0] == "0":
-                        continue
-
-                    if int(newCur) == first + second:
-                        if backTrack(i + 1, second, int(newCur)):
-                            return True
-            else:
-                for i in range(idx, len(num) - 1):
-                    newSecond = num[idx:i + 1]
-                    
-                    if len(newSecond) > 1 and newSecond[0] == "0":
-                        continue
-
-                    if backTrack(i + 1, first, int(newSecond)):
-                        return True
-
-        for i in range(len(num) - 2):
-            newNum = num[:i + 1]
-
-            if len(newNum) > 1 and newNum[0] == "0":
-                continue
             
-            if backTrack(i + 1, int(newNum), ""):
-                return True
+            for i in range(idx, len(num)):
+                n = num[idx:i + 1]
+
+                if len(n) > 1 and n[0] == "0":
+                    continue
+
+                sub.append(int(n))
+                
+                if backTrack(i + 1):
+                    return True
+                
+                sub.pop()
+
+        return backTrack(0)

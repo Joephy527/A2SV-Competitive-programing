29463eda146cb1d269c8b01d51e3291c9eb16de8
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-07-04 17:12:00 +0300

    Update partition-list.py

--- partition-list.py
+++ partition-list.py
@@ -1,31 +1,31 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
+#     def init(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         dummy = ListNode()
         dummy.next = head
-        l = dummy
-        
-        while l and l.next:
-            if l.next.val >= x:
+        cur = dummy
+
+        while cur and cur.next:
+            if cur.next.val >= x:
                 break
+                       
+            cur = cur.next
+        
+        nxt = cur.next 
 
-            l = l.next
-
-        r = l.next
-
-        while r and r.next:
-            if r.next.val < x:
-                nxt = l.next
-                rnxt = r.next
-                r.next = r.next.next
-                l.next = rnxt
-                rnxt.next = nxt
-                l = l.next
+        while nxt and nxt.next:
+            if nxt.next.val < x:
+                temp = cur.next
+                temp_next = nxt.next
+                nxt.next = nxt.next.next
+                cur.next = temp_next
+                temp_next.next = temp
+                cur = cur.next
             else:
-                r = r.next
+                nxt= nxt.next
 
         return dummy.next

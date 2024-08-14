e961f95e218689b680446e5213e44f5875cbc9f2
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-08-14 09:13:28 +0300

    Update fibonacci-number.py

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo = {0: 0, 1: 1}
+        if not n: return 0
 
-        def fibonacci(num):
-            if num not in memo:
-                memo[num] = fibonacci(num - 1) + fibonacci(num - 2)
+        memo = [0] * (n + 1)
+        memo[1] = 1
 
-            return memo[num]
+        for i in range(2, n + 1):
+            memo[i] = memo[i - 1] + memo[i - 2]
 
-        return fibonacci(n)
+        return memo[n]

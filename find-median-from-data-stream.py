18aa83ac2f6d3f7ee84e84dd36ecd99b41bf0fbd
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-08-26 08:53:41 +0300

    Create find-median-from-data-stream.py

---
+++ find-median-from-data-stream.py
@@ -0,0 +1,21 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.maxHeap, self.minHeap = [], []
+
+    def addNum(self, num):
+        if not self.maxHeap or num <= -self.maxHeap[0]:
+            heappush(self.maxHeap, -num)
+        else:
+            heappush(self.minHeap, num)
+
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+        elif len(self.minHeap) > len(self.maxHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
+
+    def findMedian(self):
+        if len(self.maxHeap) == len(self.minHeap):
+            return (-self.maxHeap[0] + self.minHeap[0]) / 2.0
+        else:
+            return -self.maxHeap[0]

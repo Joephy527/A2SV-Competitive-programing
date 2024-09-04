384981de2b5303816351afcd35f95bdf0c263538
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-09-04 10:12:22 +0300

    Update largest-number.py

--- largest-number.py
+++ largest-number.py
@@ -1,14 +1,16 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        st = [str(i) for i in nums]
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if j + 1 < len(nums):
-                    if st[j][0] < st[j + 1][0]:
-                        st[j], st[j + 1] = st[j + 1], st[j]
-                    elif st[j][0] == st[j + 1][0]:
-                        l = int("".join(st[j] + st[j+1]))
-                        r = int("".join(st[j+1] + st[j]))
-                        if l < r:
-                            st[j], st[j + 1] = st[j + 1], st[j]
-        return str(int("".join(st)))
+        nums.sort()
+
+        def isGreater(n, m):
+            return n + m > m + n
+
+
+        for i in range (len (nums)):
+            for j in range(i + 1, len(nums)):
+                if not isGreater(str(nums[i]), str(nums[j])):
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        stringNums = ''.join(map(str, nums))
+
+        return "0" if stringNums[0] == "0" else stringNums

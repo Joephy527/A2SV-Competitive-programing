3e1b94cf15c80edc9fe8e24dbd10d773385da6ea
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-05 18:50:37 +0300

    Update make-string-a-subsequence-using-cyclic-increments.py

--- make-string-a-subsequence-using-cyclic-increments.py
+++ make-string-a-subsequence-using-cyclic-increments.py
@@ -1,15 +1,14 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        p1 = p2 = 0
+        p2 = 0
 
         def checkCyclic(char1, char2):
             return (char1 == char2 or 
                     (ord(char1) + 1 - ord("a")) % 26 == ord(char2) - ord("a"))
 
-        while p1 < len(str1) and p2 < len(str2):
-            if checkCyclic(str1[p1], str2[p2]):
+        for p1 in range(len(str1)):
+            if (p2 < len(str2) and
+                checkCyclic(str1[p1], str2[p2])):
                 p2 += 1
-
-            p1 += 1
 
         return p2 == len(str2)

e794108956eeca2bbb4c25a0f03670cb85d0123f
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-10 13:07:30 +0300

    Create minimum-limit-of-balls-in-a-bag.py

---
+++ minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        nums.sort()
+        
+        def isValidMin(target):
+            totalOperations = 0
+
+            for num in nums:
+                totalOperations += math.ceil(num / target) - 1
+
+            return totalOperations <= maxOperations
+        
+        left, right = 1, max(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isValidMin(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return right

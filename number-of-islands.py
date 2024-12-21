bccc072f3b05950eece7abea9ed59a6db7819a95
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-21 18:05:41 +0300

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,30 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-        visited = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = [[False] * cols for _ in range(rows)]
         islands = 0
-        
-        def isInBound(row, col):
-            return 0 <= row < rows and 0 <= col < cols
 
-        def isLand(row, col):
-            return grid[row][col] == "1"
+        def isInbound(row, col):
+            return rows > row >= 0 <= col < cols
+
+        def isUnvisitedLand(row, col):
+            return grid[row][col] == "1" and not visited[row][col]
         
         def dfs(row, col):
-            visited.add((row, col))
+            visited[row][col] = True
 
             for x, y in directions:
-                r = row + x
-                c = col + y
+                r, c = row + x, col + y
 
-                if isInBound(r, c) and isLand(r, c) and (r, c) not in visited:
+                if isInbound(r, c) and isUnvisitedLand(r, c):
                     dfs(r, c)
 
         for r in range(rows):
             for c in range(cols):
-                if isLand(r, c) and (r, c) not in visited:
-                    islands += 1
+                if isUnvisitedLand(r, c):
                     dfs(r, c)
+                    islands += 1
 
         return islands

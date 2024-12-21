ac6a038b954d9318de93c78f5a5777af687da700
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-21 16:31:47 +0300

    Update island-perimeter.py

--- island-perimeter.py
+++ island-perimeter.py
@@ -1,28 +1,29 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
-        visited = set()
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        visited = [[False] * cols for _ in range(rows)]
 
-        def isLand(r, c):
-            return (0 <= r < rows and
-                    0 <= c < cols and
-                    grid[r][c])
+        def isInbound(row, col):
+            return rows > row >= 0 <= col < cols
 
-        def dfs(r, c):
-            visited.add((r, c))
-
+        def isLand(row, col):
+            return grid[row][col]
+        
+        def dfs(row, col):
+            visited[row][col] = True
             perimeter = 0
 
             for x, y in directions:
-                row = r + x
-                col = c + y
+                r, c = row + x, col + y
 
-                if (row, col) not in visited:
-                    if isLand(row, col):
-                        perimeter += dfs(row, col)
-                    else:
+                if isInbound(r, c):
+                    if isLand(r, c) and not visited[r][c]:
+                        perimeter += dfs(r, c)
+                    elif not visited[r][c]:
                         perimeter += 1
+                else:
+                    perimeter += 1
 
             return perimeter
 

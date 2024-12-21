b2609d19c6f583f308cd4f7b2bec0898899d93b2
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-21 17:47:56 +0300

    Update is-graph-bipartite.py

--- is-graph-bipartite.py
+++ is-graph-bipartite.py
@@ -1,24 +1,22 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        colorMap = {}
+        n = len(graph)
+        visited = [0] * n
         
         def dfs(vertex, color):
-            if vertex in colorMap:
-                if colorMap[vertex] != color:
-                    return
-                else:
-                    return True
-            
-            colorMap[vertex] = color
-
-            for u in graph[vertex]:
-                if not dfs(u, -1 * color):
+            for neigh in graph[vertex]:
+                if not visited[neigh]:
+                    visited[neigh] = -color
+                    
+                    if not dfs(neigh, -color):
+                        return
+                elif visited[neigh] == color:
                     return
 
             return True
 
-        for i in range(len(graph)):
-            if i not in colorMap and not dfs(i, -1):
-                return
+        for v in range(n):
+            if not visited[v] and not dfs(v, 1):
+                return False
 
         return True

c537c70356324604f94aaecea27380d52dce8a10
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2024-12-26 09:44:30 +0300

    Update target-sum.py

--- target-sum.py
+++ target-sum.py
@@ -1,23 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         memo = {}
-
-        def dp(idx, count):
-            if idx == len(nums):
-                if count == target:
-                    return 1
-                else: 
-                    return 0
-            
-            if (idx, count) in memo:
-                return memo[(idx, count)]
-
-            
-            include = dp(idx + 1, count + nums[idx])
-            execlude=dp(idx + 1, count - nums[idx])
-            
-            memo[(idx, count)] = include + execlude
-            
-            return  memo[(idx,count)]
         
-        return dp(0,0)
+        def backTrack(idx, total):
+            if idx >= len(nums):
+                return 1 if total == target else 0
+
+            if (idx, total) not in memo:
+                memo[(idx, total)] = (
+                    backTrack(idx + 1, total + nums[idx])
+                    +
+                    backTrack(idx + 1, total - nums[idx])
+                )
+
+            return memo[(idx, total)]
+
+        return backTrack(0, 0)

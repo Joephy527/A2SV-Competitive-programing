358c5c52de44386deb3ddf3e4a524fdb6f13d02f
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2025-01-09 06:44:45 +0300

    Update count-prefix-and-suffix-pairs-i.py

--- count-prefix-and-suffix-pairs-i.py
+++ count-prefix-and-suffix-pairs-i.py
@@ -1,9 +1,30 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        node = self.root
+
+        for first_char, last_char in zip(word, reversed(word)):
+            if (first_char, last_char) not in node.children:
+                node.children[(first_char, last_char)] = TrieNode()
+
+            node = node.children[(first_char, last_char)]
+            node.count += 1
+
+        return node.count - 1
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        trie = Trie()
         pairs = 0
-        
-        for i in range(len(words)):
-            for j in range(i + 1, len(words)):
-                pairs += words[j].startswith(words[i]) and words[j].endswith(words[i])
+
+        for word in reversed(words):
+            pairs += trie.add(word)
 
         return pairs

05e7adacf6003b44f8abeddeb3ea38303606c68e
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2025-02-14 14:20:05 +0300

    Create product-of-the-last-k-numbers.py

---
+++ product-of-the-last-k-numbers.py
@@ -0,0 +1,25 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix = [1]
+        self.n = 0
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix = [1]
+            self.n = 0
+        else:
+            self.prefix.append(self.prefix[-1] * num)
+            self.n += 1
+
+    def getProduct(self, k: int) -> int:
+        if k > self.n:
+            return 0
+
+        return self.prefix[-1] // self.prefix[self.n - k]
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)

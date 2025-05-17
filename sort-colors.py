86bd01516653e1dc0521442f92055df609b26ee1
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2025-05-17 12:06:19 +0300

    Update sort-colors.py

--- sort-colors.py
+++ sort-colors.py
@@ -3,7 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] < nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i]
+        left = cur = 0
+        right = len(nums) - 1
+
+        while cur <= right:
+            if nums[cur] == 0:
+                nums[cur], nums[left] = nums[left], nums[cur]
+                left += 1
+                cur += 1
+            elif nums[cur] == 2:
+                nums[cur], nums[right] = nums[right], nums[cur]
+                right -= 1
+            else:
+                cur += 1

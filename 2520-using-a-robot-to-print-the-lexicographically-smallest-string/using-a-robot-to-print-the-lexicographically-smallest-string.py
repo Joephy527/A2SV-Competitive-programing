5768748f421971fb24ce724d86f795c95e0c80b4
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-06-13 12:53:15 +0300

    Time: 564 ms (33.52%) | Memory: 20.6 MB (71.92%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,22 +1,20 @@
 class Solution:
     def robotWithString(self, s: str) -> str:
         count = Counter(s)
-        min_char = 0
-        stack, p = [], []
-
+        min_char = ord("a")
+        smallest, stack = [], []
+    
         for c in s:
             stack.append(c)
             count[c] -= 1
 
             while (
-                min_char != ord("z") and
+                min_char < ord("z") and
                 not count[chr(min_char)]
             ):
                 min_char += 1
 
-            min_character = chr(min_char)
+            while stack and stack[-1] <= chr(min_char):
+                smallest.append(stack.pop())
 
-            while stack and stack[-1] <= min_character:
-                p.append(stack.pop())
-
-        return "".join(p)
+        return "".join(smallest)

2127a026a146cab2318b1bc77e166eefaf9aa1bb
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-01 17:44:27 +0300

    Time: 146 ms (49.37%) | Memory: 78 MB (98.26%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,62 +1,26 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dict = {}
+        self.dict = OrderedDict()
         self.cap = capacity
-        self.left, self.right = ListNode(), ListNode()
-        self.left.next, self.right.prev = self.right, self.left
 
     def get(self, key: int) -> int:
         if key in self.dict:
-            node = self.dict[key]
-            val = node.val
-            
-            self.remove(node)
-            self.insert(node)
+            self.dict.move_to_end(key)
 
-            return val
+            return self.dict[key]
 
         return -1
 
 
     def put(self, key: int, value: int) -> None:
         if key in self.dict:
-            node = self.dict[key]
-            node.val = value
-            self.remove(node)
-        else:
-            node = ListNode(value, key)
-            self.dict[key] = node
+            self.dict.move_to_end(key)
         
-        self.insert(node)
+        self.dict[key] = value
 
         if len(self.dict) > self.cap:
-            to_be_removed = self.left.next
-            self.remove(to_be_removed)
-            del self.dict[to_be_removed.key]
-
-    
-    def insert(self, node):
-        prev = self.right.prev
-        self.right.prev = node
-        prev.next = node
-        node.prev = prev
-        node.next = self.right
-
-    
-    def remove(self, node):
-        nxt = node.next
-        prev = node.prev
-        nxt.prev = prev
-        prev.next = nxt
-
-
-class ListNode:
-    def __init__(self, val = 0, key = -1):
-        self.val = val
-        self.key = key
-        self.prev = None
-        self.next = None
+            self.dict.popitem(last=False)
 
 
 

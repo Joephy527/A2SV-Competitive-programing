f2decab69363afe49fedd5436ed6db4fbdca4698
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-03 00:42:48 +0300

    Time: 40 ms (98.40%) | Memory: 22.1 MB (71.46%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -9,30 +9,17 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def dfs(node):
             if not node:
-                return node, False, False
+                return
 
-            left, is_node_left, is_parent_left = dfs(node.left)
-            right, is_node_right, is_parent_right = dfs(node.right)
+            if node in (p, q):
+                return node
 
-            if is_parent_left or is_parent_right:
-                val = left if is_parent_left else right
-                return val, False, True
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-            val = node
-            is_cur = node in (p, q)
-            is_node = is_node_left or is_node_right or is_cur
-            
-            if (
-                (is_node_left and is_node_right) or
-                (is_node_left or is_node_right) and is_cur
-            ):
-                return val, is_node, True
+            if left and right:
+                return node
 
-            if is_node_left:
-                val = left
-            elif is_node_right:
-                val = right
+            return left or right
 
-            return val, is_node, False
-
-        return dfs(root)[0]
+        return dfs(root)

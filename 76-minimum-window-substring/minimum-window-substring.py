9673bcbd88eced5ce579c5a6178cbef24ad0afec
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-03 23:36:38 +0300

    Time: 81 ms (50.96%) | Memory: 18.1 MB (93.20%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,25 +2,27 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         count_t = Counter(t)
         count_s = defaultdict(int)
-        count = defaultdict(int)
-        p = 0
+        p = start = end = valid = 0
         minimum = float("inf")
-        start = end = 0
-
-        def is_substring():
-            return all(
-                count_s[c] >= count_t[c] for c in count_t
-            )
 
         for seek in range(len(s)):
-            count_s[s[seek]] += 1
+            if s[seek] in count_t:
+                count_s[s[seek]] += 1
+                
+                if count_s[s[seek]] == count_t[s[seek]]:
+                    valid += 1
 
-            while p <= seek and count_s[s[p]] > count_t[s[p]]:
-                count_s[s[p]] -= 1
+            while valid == len(count_t):
+                if minimum > seek - p + 1:
+                    minimum = seek - p + 1
+                    start, end = p, seek + 1
+
+                if s[p] in count_t:
+                    count_s[s[p]] -= 1
+                    
+                    if count_s[s[p]] < count_t[s[p]]:
+                        valid -= 1
+
                 p += 1
-
-            if is_substring() and minimum > seek - p + 1:
-                minimum = seek - p + 1
-                start, end = p, seek + 1
 
         return s[start:end] if minimum != float("inf") else ""

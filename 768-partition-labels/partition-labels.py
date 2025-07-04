9ede6eddab1e9dd8420d6511ee618f84420e54f3
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-04 23:34:11 +0300

    Time: 5 ms (53.51%) | Memory: 18 MB (12.13%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,11 +1,8 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        idx_map = {}
+        idx_map = {c: i for i, c in enumerate(s)}
         last_seen = p = 0
         partitions = []
-
-        for i, c in enumerate(s):
-            idx_map[c] = i
 
         for seek in range(len(s)):
             last_seen = max(last_seen, idx_map[s[seek]])

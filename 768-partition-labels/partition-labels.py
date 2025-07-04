1b85250aad78fa667ba305780846f8902b9f7b18
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-04 23:31:16 +0300

    Time: 4 ms (63.95%) | Memory: 17.7 MB (53.54%) - LeetSync

---
+++ 768-partition-labels/partition-labels.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        idx_map = {}
+        p = 0
+        partitions = []
+        last_seen = 0
+
+        for i, c in enumerate(s):
+            idx_map[c] = i
+
+        for seek in range(len(s)):
+            last_seen = max(last_seen, idx_map[s[seek]])
+            
+            if seek == last_seen:
+                partitions.append(seek - p + 1)
+                p = seek + 1
+
+        return partitions

3a4d75a6301e5745ef7eec9eac89cda9cde9cecc
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-07 19:23:19 +0300

    Time: 1490 ms (33.15%) | Memory: 25.5 MB (8.57%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,32 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        
-        if n == 1: return s
-
-        def find_palindrome(start, end):
-            while (
-                start >= 0 and
-                end < n and
-                s[start] == s[end]
-            ):
-                start -= 1
-                end += 1
-
-            return start, end
-
+        memo = [[False] * n for _ in range(n)]
         max_left = max_right = 0
 
-        for idx in range(n):
-            max_dif = max_right - max_left
+        for end in range(n):
+            for start in range(end + 1):
+                if (
+                    s[start] == s[end] and
+                    (
+                        end - start < 3 or
+                        memo[start + 1][end - 1]
+                    )
+                ):
+                    memo[start][end] = True
 
-            left_one, right_one = find_palindrome(idx, idx)
-            left_two, right_two = find_palindrome(idx, idx + 1)
+                    if end - start > max_right - max_left:
+                        max_left, max_right = start, end
 
-            if right_one - left_one > max_dif:
-                max_left, max_right = left_one, right_one
-            
-            if right_two - left_two > max_dif:
-                max_left, max_right = left_two, right_two
-
-        return s[max_left + 1:max_right]
+        return s[max_left:max_right + 1]

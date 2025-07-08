5a28696000dddc96beada146dbff933662741927
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-08 06:43:30 +0300

    Time: 9 ms (14.18%) | Memory: 18 MB (14.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -11,30 +11,18 @@ class Solution:
         length1 = length2 = 0
         cur1, cur2 = l1, l2
 
-        while cur1 and cur2:
-            cur_val = cur1.val + cur2.val + rem
-            cur_node = ListNode(cur_val % 10)
-            rem = cur_val // 10
-            cur.next = cur_node
-            cur = cur.next
-            cur1 = cur1.next
-            cur2 = cur2.next
+        while cur1 or cur2:
+            cur_val = rem
+            cur_val += cur1.val if cur1 else 0
+            cur_val += cur2.val if cur2 else 0
 
-        while cur1:
-            cur_val = cur1.val + rem
-            cur_node = ListNode(cur_val % 10)
             rem = cur_val // 10
-            cur.next = cur_node
+            
+            cur.next = ListNode(cur_val % 10)
             cur = cur.next
-            cur1 = cur1.next
-
-        while cur2:
-            cur_val = cur2.val + rem
-            cur_node = ListNode(cur_val % 10)
-            rem = cur_val // 10
-            cur.next = cur_node
-            cur = cur.next
-            cur2 = cur2.next
+            
+            cur1 = cur1.next if cur1 else cur1
+            cur2 = cur2.next if cur2 else cur2
 
         if rem:
             cur.next = ListNode(rem)

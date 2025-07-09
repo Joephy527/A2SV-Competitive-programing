8ea144b24fac3c03d7c159bcc3b5bedf793809a9
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-09 18:25:12 +0300

    Time: 160 ms (33.04%) | Memory: 77.9 MB (95.00%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -15,35 +15,26 @@ class LRUCache:
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def remove(self) -> ListNode:
-        cur = self.head.next
-        self.head.next = cur.next
-        cur.next.prev = self.head
-
-        return cur
+    def remove(self, node: ListNode) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
 
-    def insert(self, key: int, is_old: bool = True) -> ListNode:
-        node = self.cache[key]
-
-        if is_old:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-
+    def insert(self, node: ListNode) -> None:
         prev = self.tail.prev
         prev.next = node
         node.next = self.tail
         node.prev = prev
         self.tail.prev = node
 
-        return node
-
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
-        node = self.insert(key)
+        node = self.cache[key]
+        self.remove(node)
+        self.insert(node)
         
         return node.val
 
@@ -52,14 +43,16 @@ class LRUCache:
         if key not in self.cache:
             node = ListNode(val=value, key=key)
             self.cache[key] = node
-            self.insert(key, False)
         else:
             node = self.cache[key]
             node.val = value
-            self.insert(key)
+            self.remove(node)
+        
+        self.insert(node)
 
         if len(self.cache) > self.cap:
-            node = self.remove()
+            node = self.head.next
+            self.remove(node)
             del self.cache[node.key]
 
 

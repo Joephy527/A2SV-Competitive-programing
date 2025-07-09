d91d5e6d0531cf21e867da159e429c105fff849e
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-09 18:19:50 +0300

    Time: 178 ms (18.09%) | Memory: 77.9 MB (95.00%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,24 +1,66 @@
+class ListNode:
+    def __init__(self, val=0, key=-1, prev=None, nxt=None):
+        self.key = key
+        self.prev = prev
+        self.next = nxt
+        self.val = val
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
+        self.cache = {}
         self.cap = capacity
-        
+        self.head, self.tail = ListNode(), ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self) -> ListNode:
+        cur = self.head.next
+        self.head.next = cur.next
+        cur.next.prev = self.head
+
+        return cur
+
+
+    def insert(self, key: int, is_old: bool = True) -> ListNode:
+        node = self.cache[key]
+
+        if is_old:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+        prev = self.tail.prev
+        prev.next = node
+        node.next = self.tail
+        node.prev = prev
+        self.tail.prev = node
+
+        return node
+
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        node = self.insert(key)
+        
+        return node.val
 
 
     def put(self, key: int, value: int) -> None:
-        self.cache[key] = value
-        self.cache.move_to_end(key)
+        if key not in self.cache:
+            node = ListNode(val=value, key=key)
+            self.cache[key] = node
+            self.insert(key, False)
+        else:
+            node = self.cache[key]
+            node.val = value
+            self.insert(key)
 
         if len(self.cache) > self.cap:
-            self.cache.popitem(last=False)
+            node = self.remove()
+            del self.cache[node.key]
 
 
 

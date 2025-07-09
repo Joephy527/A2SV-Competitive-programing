637ce9e98cbc4ddb7923668b7269242ec3e27d85
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-09 19:46:46 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (18.14%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,29 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows, cols = len(matrix), len(matrix[0])
-        visited = [[False] * cols for _ in range(rows)]
-        directions = [(0, 1), (1, 0), (0, -1), (-1 , 0)]
+        top = left = 0
+        bottom, right = len(matrix), len(matrix[0])
         spiral = []
 
-        def is_valid(row, col):
-            return (
-                rows > row >= 0 <= col < cols and
-                not visited[row][col]
-            )
-
-        def get_spiral(row, col, d, t):
-            if not visited[row][col]:
-                spiral.append(matrix[row][col])
-                visited[row][col] = True
-
-            x, y = directions[d]
-            r, c = row + x, col + y
+        while top < bottom and left < right:
+            for i in range(left, right):
+                spiral.append(matrix[top][i])
             
-            if is_valid(r, c):
-                get_spiral(r, c, d, 0)
-            else:
-                if t >= 4: return
-                get_spiral(row, col, (d + 1) % 4, t + 1)
+            top += 1
 
-        get_spiral(0, 0, 0, 0)
+            for i in range(top, bottom):
+                spiral.append(matrix[i][right - 1])
+            
+            right -= 1
+
+            if top >= bottom or left >= right:
+                break
+
+            for i in range(right - 1, left - 1, -1):
+                spiral.append(matrix[bottom - 1][i])
+
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                spiral.append(matrix[i][left])
+
+            left += 1
+
         return spiral

8328cc8f4db283466937535bcf2e618ec950db0c
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-10 15:38:33 +0300

    Time: 6 ms (29.50%) | Memory: 17.8 MB (71.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,33 +3,36 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         queue = deque()
-        minutes = fresh = 0
-
-        def is_inbound(row, col):
-            return rows > row >= 0 <= col < cols
+        fresh_count = minute = 0
 
         def is_fresh(row, col):
-            return grid[row][col] == 1
+            return (
+                rows > row >= 0 <= col < cols and
+                grid[row][col] == 1
+            )
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 2:
-                    queue.append((row, col, 0))
-                if grid[row][col] == 1:
-                    fresh += 1
-
-        if not fresh: return 0
+                    queue.append((row, col))
+                elif grid[row][col] == 1:
+                    fresh_count += 1
 
         while queue:
-            row, col, prev_minute = queue.popleft()
-            minutes = prev_minute
+            if not fresh_count:
+                return minute
 
-            for x, y in directions:
-                r, c = row + x, col + y
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
 
-                if is_inbound(r, c) and is_fresh(r, c):
-                    fresh -= 1
-                    grid[r][c] = 2
-                    queue.append((r, c, prev_minute + 1))
+                for x, y in directions:
+                    r, c = row + x, col + y
 
-        return -1 if fresh else minutes
+                    if is_fresh(r, c):
+                        grid[r][c] = 2
+                        queue.append((r, c))
+                        fresh_count -= 1
+
+            minute += 1
+
+        return minute if not fresh_count else -1

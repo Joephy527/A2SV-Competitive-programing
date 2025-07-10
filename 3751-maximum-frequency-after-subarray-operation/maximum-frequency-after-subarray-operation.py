13a0d3cf299b148e4c4ef808bc2b914d3f81c101
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-10 13:40:50 +0300

    Time: 216 ms (87.66%) | Memory: 24.5 MB (5.16%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
@@ -1,27 +1,29 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        count, last, first = [0] * 51, [-1] * 51, [-1] * 51
-        prefix = [0]
-        max_count = 0
+        n = len(nums)
+        count = defaultdict(int)
+        seen = {}
+        prefix = [0] * n
+        prefix[0] = int(nums[0] == k)
+        max_k = 0
+
+        for idx in range(n):
+            prefix[idx] = prefix[idx - 1] + int(nums[idx] == k)
 
         for idx, num in enumerate(nums):
-            prefix.append(prefix[-1] + int(num == k))
-
-            if first[num] == -1:
-                first[num] = idx
+            if num not in seen:
+                seen[num] = idx
 
         for idx, num in enumerate(nums):
-            if num == k: continue
+            if num != k:
+                count[num] += 1
+                last_seen = seen[num]
+                k_count = prefix[idx] - prefix[last_seen]
+                k_freq = count[num] - k_count
+                max_k = max(max_k, k_freq)
 
-            count[num] += 1
-            last[num] = idx
-            k_count = prefix[idx + 1] - prefix[first[num] + 1]
-            net = count[num] - k_count
+                if k_freq <= 0:
+                    seen[num] = idx
+                    count[num] = 1
 
-            if net <= 0:
-                first[num] = idx
-                count[num] = 1
-
-            max_count = max(max_count, net)
-
-        return max_count + prefix[-1]
+        return max_k + prefix[-1]

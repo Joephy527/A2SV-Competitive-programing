e0f6d1daa854faeaa610d7eb08e6ade4fac3b6ae
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-12 17:48:01 +0300

    Time: 91 ms (99.07%) | Memory: 31.2 MB (39.25%) - LeetSync

--- 2549-next-greater-element-iv/next-greater-element-iv.py
+++ 2549-next-greater-element-iv/next-greater-element-iv.py
@@ -1,16 +1,17 @@
 class Solution:
     def secondGreaterElement(self, nums: List[int]) -> List[int]:
-        first, second = [], []
+        first, second, temp = [], [], []
         greater = [-1] * len(nums)
 
         for idx, num in enumerate(nums):
-            while second and nums[second[0][1]] < num:
-                _, i = heappop(second)
-                greater[i] = num
+            while second and nums[second[-1]] < num:
+                greater[second.pop()] = num
 
             while first and nums[first[-1]] < num:
-                i = first.pop()
-                heappush(second, (nums[i], i))
+                temp.append(first.pop())
+
+            while temp:
+                second.append(temp.pop())
 
             first.append(idx)
 

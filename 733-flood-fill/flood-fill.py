274b8f9acff337ee90bd7b6f2eaf864bbd710a62
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-13 00:52:46 +0300

    Time: 3 ms (16.15%) | Memory: 18.1 MB (28.03%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,25 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if image[sr][sc] != color:
-            rows, cols = len(image), len(image[0])
-            directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            original = image[sr][sc]
+        rows, cols = len(image), len(image[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        starting = image[sr][sc]
 
-            def is_inbound(row, col):
-                return rows > row >= 0 <= col < cols
+        def is_valid(row, col):
+            return (
+                rows > row >= 0 <= col < cols and
+                image[row][col] == starting
+            )
 
-            def is_not_visited(row, col):
-                return image[row][col] == original
+        def dfs(row, col):
+            image[row][col] = color
 
-            def dfs(row, col):
-                image[row][col] = color
-
-                for x, y in directions:
-                    r, c = row + x, col + y
-
-                    if is_inbound(r, c) and is_not_visited(r, c):
-                        dfs(r, c)
-
+            for x, y in directions:
+                r, c = row + x, col + y
+                if is_valid(r, c):
+                    dfs(r, c)
+        
+        if starting != color:
             dfs(sr, sc)
 
         return image

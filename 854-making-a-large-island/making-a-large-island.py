1a46b602b62a13cee5862c9b2e200be73d2aa809
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-16 00:43:38 +0300

    Time: 1127 ms (38.75%) | Memory: 28.9 MB (58.04%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -2,39 +2,49 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        area_map = defaultdict(int)
+        areas = defaultdict(int)
+        areas[0] = 1
+        islands = 2
+        max_area = 1
 
-        def in_bound(row, col):
+        def is_inbound(row, col):
             return rows > row >= 0 <= col < cols
 
         def is_land(row, col):
             return grid[row][col] == 1
 
         def dfs(row, col):
-            grid[row][col] = -1
+            grid[row][col] = islands
+            areas[islands] += 1
+
+            for x, y in directions:
+                r, c = row + x, col + y
+
+                if is_inbound(r, c) and is_land(r, c):
+                    dfs(r, c)
+
+        def get_area(row, col):
+            seen = set()
             area = 1
 
             for x, y in directions:
-                r, c = x + row, y + col
+                r, c = row + x, col + y
 
-                if in_bound(r, c):
-                    if is_land(r, c):
-                        area += dfs(r, c)
-                    elif not grid[r][c]:
-                        water.add((r, c))
+                if is_inbound(r, c) and grid[r][c] > 0 and grid[r][c] not in seen:
+                    seen.add(grid[r][c])
+                    area += areas[grid[r][c]]
 
             return area
 
         for row in range(rows):
             for col in range(cols):
                 if is_land(row, col):
-                    water = set()
-                    area = dfs(row, col)
+                    dfs(row, col)
+                    islands += 1
 
-                    for cell in water:
-                        area_map[cell] += area
+        for row in range(rows):
+            for col in range(cols):
+                if not grid[row][col]:
+                    max_area = max(max_area, get_area(row, col))
 
-        if area_map:
-            return 1 + max(area_map.values())
-
-        return rows * cols if grid[0][0] else 1
+        return max(max_area, max(areas.values()))

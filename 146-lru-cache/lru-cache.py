1c6457facb3f6111993ae1781ac912a86042d73a
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-15 19:38:11 +0300

    Time: 161 ms (31.91%) | Memory: 78 MB (92.07%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,57 +1,58 @@
 class ListNode:
-    def __init__(self, val=0, key=-1, prev=None, nxt=None):
+    def __init__(self, val = -1, key = -1):
         self.key = key
-        self.prev = prev
-        self.next = nxt
         self.val = val
-
+        self.prev = None
+        self.nxt = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
         self.cap = capacity
-        self.head, self.tail = ListNode(), ListNode()
-        self.head.next = self.tail
+        self.cache = {}
+        self.head = self.tail = ListNode()
+        self.head.nxt = self.tail
         self.tail.prev = self.head
 
+
     def remove(self, node: ListNode) -> None:
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        node.prev.nxt = node.nxt
+        node.nxt.prev = node.prev
 
 
     def insert(self, node: ListNode) -> None:
         prev = self.tail.prev
-        prev.next = node
-        node.next = self.tail
-        node.prev = prev
         self.tail.prev = node
+        prev.nxt = node
+        node.nxt = self.tail
+        node.prev = prev
 
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        
+
         node = self.cache[key]
+
         self.remove(node)
         self.insert(node)
-        
+
         return node.val
 
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            node = ListNode(val=value, key=key)
-            self.cache[key] = node
-        else:
+        if key in self.cache:
             node = self.cache[key]
             node.val = value
             self.remove(node)
-        
+        else:
+            node = ListNode(value, key)
+            self.cache[key] = node
+
         self.insert(node)
 
         if len(self.cache) > self.cap:
-            node = self.head.next
+            node = self.head.nxt
             self.remove(node)
             del self.cache[node.key]
 

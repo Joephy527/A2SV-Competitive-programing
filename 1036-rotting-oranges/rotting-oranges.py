b12ec2dc618075264dd8a792e0abf851d0ee01d0
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-15 18:41:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,13 +2,13 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        fresh = minutes = 0
         queue = deque()
-        fresh_count = minute = 0
 
         def is_fresh(row, col):
             return (
-                rows > row >= 0 <= col < cols and
-                grid[row][col] == 1
+                rows > row >= 0 <= col < cols
+                and grid[row][col] == 1
             )
 
         for row in range(rows):
@@ -16,20 +16,20 @@ class Solution:
                 if grid[row][col] == 2:
                     queue.append((row, col))
                 elif grid[row][col] == 1:
-                    fresh_count += 1
+                    fresh += 1
 
-        while queue and fresh_count:
+        while queue and fresh:
             for _ in range(len(queue)):
                 row, col = queue.popleft()
 
                 for x, y in directions:
-                    r, c = row + x, col + y
+                    r, c = x + row, y + col
 
                     if is_fresh(r, c):
                         grid[r][c] = 2
+                        fresh -= 1
                         queue.append((r, c))
-                        fresh_count -= 1
 
-            minute += 1
+            minutes += 1
 
-        return minute if not fresh_count else -1
+        return minutes if not fresh else -1

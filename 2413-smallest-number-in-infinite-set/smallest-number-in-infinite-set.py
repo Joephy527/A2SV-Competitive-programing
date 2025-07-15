9dbdc63874089513567e13dca35cae9c8626b065
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-15 13:09:04 +0300

    Time: 10 ms (91.33%) | Memory: 18.6 MB (13.21%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
@@ -3,22 +3,27 @@ class SmallestInfiniteSet:
     def __init__(self):
         self.heap = []
         self.min_int = 1
+        self.seen = [False] * 1001
         
 
     def popSmallest(self) -> int:
         if self.heap:
-            return heappop(self.heap)
+            cur_min = heappop(self.heap)
+        else:
+            cur_min = self.min_int
+            self.min_int += 1
 
-        self.min_int += 1
+        self.seen[cur_min] = False
 
-        return self.min_int - 1
+        return cur_min
 
     def addBack(self, num: int) -> None:
         if (
             num < self.min_int and
-            num not in self.heap
+            not self.seen[num]
         ):
             heappush(self.heap, num)
+            self.seen[num] = True
 
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:

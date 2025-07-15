2e7611b6765a1e3c8b372792e0b1c730642ef5e5
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-07-16 00:25:06 +0300

    Time: 67 ms (73.10%) | Memory: 18.4 MB (16.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,27 +2,26 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         count_t = Counter(t)
         count_s = defaultdict(int)
-        p = start = end = valid = 0
-        minimum = float("inf")
+        included = p = 0
+        substring = float("inf")
+        left = right = 0
 
         for seek in range(len(s)):
-            if s[seek] in count_t:
-                count_s[s[seek]] += 1
+            count_s[s[seek]] += 1
+            
+            if count_s[s[seek]] == count_t[s[seek]]:
+                included += 1
+
+            while included == len(count_t):
+                if substring > seek - p + 1:
+                    substring = seek - p + 1
+                    left, right = p, seek
                 
-                if count_s[s[seek]] == count_t[s[seek]]:
-                    valid += 1
-
-            while valid == len(count_t):
-                if minimum > seek - p + 1:
-                    minimum = seek - p + 1
-                    start, end = p, seek + 1
-
-                if s[p] in count_t:
-                    count_s[s[p]] -= 1
-                    
-                    if count_s[s[p]] < count_t[s[p]]:
-                        valid -= 1
+                count_s[s[p]] -= 1
+                
+                if count_s[s[p]] < count_t[s[p]]:
+                    included -= 1
 
                 p += 1
 
-        return s[start:end] if minimum != float("inf") else ""
+        return s[left:right + 1] if substring != float("inf") else ""

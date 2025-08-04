3693cb8620b9a6a7b6599b33ca0ff1a1de0739a1
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-04 14:36:41 +0300

    Time: 200 ms (40.82%) | Memory: 22.1 MB (54.45%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,18 +1,17 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         left, right = max(weights), sum(weights)
-        weight = right
 
         def is_shippable(weight):
             day = 1
             s = 0
 
             for w in weights:
-                if s + w <= weight:
-                    s += w
-                else:
-                    s = w
+                if s + w > weight:
+                    s = 0
                     day += 1
+
+                s += w
 
             return day <= days
 
@@ -20,9 +19,8 @@ class Solution:
             mid = left + (right - left) // 2
 
             if is_shippable(mid):
-                weight = mid
                 right = mid - 1
             else:
                 left = mid + 1
 
-        return weight
+        return left

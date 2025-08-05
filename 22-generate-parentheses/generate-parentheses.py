125ab3ac5ef998cf9634619d0b8816b2baa41cdf
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-06 00:02:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.21%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        combinations = []
+        parentheses = []
+
+        def back_track(opened = 0, closed = 0):
+            if opened + closed == n * 2:
+                combinations.append("".join(parentheses))
+
+                return
+
+            if opened < n:
+                parentheses.append("(")
+                back_track(opened + 1, closed)
+                parentheses.pop()
+
+            if opened > closed:
+                parentheses.append(")")
+                back_track(opened, closed + 1)
+                parentheses.pop()
+
+        back_track()
+
+        return combinations

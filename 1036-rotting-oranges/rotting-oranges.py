7b604114f55fb323b5acf3d4569dbd838cecc60f
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-06 14:09:28 +0300

    Time: 6 ms (29.69%) | Memory: 17.7 MB (92.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,34 +2,32 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        fresh = minutes = 0
         queue = deque()
+        fresh_count = minute = 0
 
-        def is_fresh(row, col):
-            return (
-                rows > row >= 0 <= col < cols
-                and grid[row][col] == 1
-            )
+        def is_inbound(row, col):
+            return rows > row >= 0 <= col < cols
 
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 2:
                     queue.append((row, col))
-                elif grid[row][col] == 1:
-                    fresh += 1
 
-        while queue and fresh:
+                if grid[row][col] == 1:
+                    fresh_count += 1
+
+        while queue and fresh_count:
+            minute += 1
+            
             for _ in range(len(queue)):
                 row, col = queue.popleft()
 
                 for x, y in directions:
-                    r, c = x + row, y + col
-
-                    if is_fresh(r, c):
+                    r, c = row + x, col + y
+                    
+                    if is_inbound(r, c) and grid[r][c] == 1:
+                        fresh_count -= 1
                         grid[r][c] = 2
-                        fresh -= 1
                         queue.append((r, c))
 
-            minutes += 1
-
-        return minutes if not fresh else -1
+        return -1 if fresh_count else minute

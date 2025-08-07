c4891588eb3c9e27c678b77903336f3235992b70
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-07 11:18:34 +0300

    Time: 150 ms (96.43%) | Memory: 68.4 MB (92.23%) - LeetSync

--- 2833-count-zero-request-servers/count-zero-request-servers.py
+++ 2833-count-zero-request-servers/count-zero-request-servers.py
@@ -1,25 +1,30 @@
 class Solution:
     def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
-        logs.sort(key=lambda x: x[1])
-        
-        queries= sorted([(q,i) for i, q in enumerate(queries)])
-        window = defaultdict(int)
+        logs.sort(key=lambda log: log[1])
+        queries = sorted(enumerate(queries), key=lambda query: query[1])
         active = left = right = 0
+        window = [0] * (n + 1)
         in_active = [0] * len(queries)
 
-        for end, idx in queries:
+        for idx, end in queries:
             start = end - x
-            
-            while right < len(logs) and logs[right][1] <= end:
+
+            while (
+                right < len(logs) and
+                logs[right][1] <= end
+            ):
                 window[logs[right][0]] += 1
                 active += (window[logs[right][0]] == 1)
                 right += 1
-            
-            while left < len(logs) and logs[left][1] < start:
+
+            while (
+                left < len(logs) and
+                logs[left][1] < start
+            ):
                 window[logs[left][0]] -= 1
                 active -= (window[logs[left][0]] == 0)
                 left += 1
-            
+
             in_active[idx] = n - active
-        
+
         return in_active

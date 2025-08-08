a1716ba4aab56331115103b77e398663c52606a6
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-08 18:38:22 +0300

    Time: 886 ms (98.66%) | Memory: 35.9 MB (5.66%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -2,25 +2,13 @@ class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
         sum_1, sum_2 = sum(nums1), sum(nums2)
         zero_1, zero_2 = nums1.count(0), nums2.count(0)
+        max_1, max_2 = sum_1 + zero_1, sum_2 + zero_2
 
-        print(sum_1, zero_1)
-        print(sum_2, zero_2)
+        if not zero_1 and not zero_2:
+            return sum_1 if sum_1 == sum_2 else -1
+        elif not zero_1:
+            return sum_1 if max_2 <= sum_1 else -1
+        elif not zero_2:
+            return sum_2 if max_1 <= sum_2 else -1
 
-        if (
-            (sum_1 > sum_2 and not zero_2) or
-            (sum_1 < sum_2 and not zero_1) or
-            (sum_1 == sum_2 and (
-                (zero_1 and not zero_2) or
-                (zero_2 and not zero_1)
-            ))
-        ):
-            return -1
-
-        if sum_1 > sum_2:
-            if zero_2 > sum_1 - sum_2 and not zero_1:
-                return -1
-        elif sum_1 < sum_2:
-            if zero_1 > sum_2 - sum_1 and  not zero_2:
-                return -1
-
-        return max(sum_1 + zero_1, sum_2 + zero_2)
+        return max(max_1, max_2)

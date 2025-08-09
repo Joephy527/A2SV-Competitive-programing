536711eea6fb854eb212994eeaf8b8c9f75fb326
Yoseph Ephrem Kifle <joephy527@gmail.com>
2025-08-09 22:54:10 +0300

    Time: 19 ms (68.47%) | Memory: 18.8 MB (58.32%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-        directions = [0, 1, -1]
         prev = matrix[0]
+        print(prev)
 
         for r in range(1, rows):
-            cur = [float("inf")] * cols
+            cur = prev[:]
 
             for c in range(cols):
                 if c < cols - 1:
@@ -14,7 +14,7 @@ class Solution:
                 if c > 0:
                     cur[c] = min(cur[c], prev[c - 1])
 
-                cur[c] = matrix[r][c] + min(cur[c], prev[c])
+                cur[c] += matrix[r][c]
 
             prev = cur
 

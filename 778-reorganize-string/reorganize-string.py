737b343c5e1d0d4009d7d0162c982b521918eb3d
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-06-27 17:01:10 +0300

    Time: 3 ms (52.34%) | Memory: 19.4 MB (51.50%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -3,28 +3,25 @@ class Solution:
         counts = Counter(s)
         max_heap = [(-count, char) for char, count in counts.items()]
         rearrangement = []
+        prev_count, prev_char = 0, ""
 
         heapify(max_heap)
 
         while max_heap:
             cnt, char = heappop(max_heap)
             
-            if not cnt:
-                continue
-            
+            if char == prev_char:
+                return ""
+
             rearrangement.append(char)
 
-            if cnt < -1:
-                if not max_heap:
-                    return ""
+            if prev_count < 0:
+                heappush(max_heap, (prev_count, prev_char))
+            
+            if cnt <= -1:
+                prev_count, prev_char = cnt + 1, char
 
-                cnt_2, char_2 = heappop(max_heap)
-                
-                if not cnt_2:
-                    return ""
-                
-                rearrangement.append(char_2)
-                heappush(max_heap, (cnt_2 + 1, char_2))
-                heappush(max_heap, (cnt + 1, char))
+        if prev_count <= -1:
+            return ""
 
         return "".join(rearrangement)

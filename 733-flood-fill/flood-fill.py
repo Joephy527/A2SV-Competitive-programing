3aa849440ab48d9dfdfb2f1344c9628e37d120b4
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-01 04:03:06 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (50.20%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -2,24 +2,25 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         rows, cols = len(image), len(image[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        starting = image[sr][sc]
+        original_color = image[sr][sc]
 
-        def is_valid(row, col):
-            return (
-                rows > row >= 0 <= col < cols and
-                image[row][col] == starting and
-                image[row][col] != color
-            )
+        if original_color == color:
+            return image
 
-        def dfs(row, col):
-            image[row][col] = color
+        image[sr][sc] = color
+        queue = deque([(sr, sc)])
+
+        def is_inbound(row, col):
+            return rows > row >= 0 <= col < cols 
+
+        while queue:
+            row, col = queue.popleft()
 
             for x, y in directions:
                 r, c = row + x, col + y
                 
-                if is_valid(r, c):
-                    dfs(r, c)
-
-        dfs(sr, sc)
+                if is_inbound(r, c) and image[r][c] == original_color:
+                    image[r][c] = color
+                    queue.append((r, c))
 
         return image

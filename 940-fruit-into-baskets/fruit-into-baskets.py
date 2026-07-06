671ced8456838c59f4cd778695486155d6f3daa1
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-06 16:41:58 +0300

    Time: 189 ms (51.52%) | Memory: 25.7 MB (84.71%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -5,15 +5,18 @@ class Solution:
 
         for s in range(len(fruits)):
             count[fruits[s]] += 1
-            
+
             while len(count) > 2:
                 count[fruits[p]] -= 1
                 
-                if not count[fruits[p]]:
+                if count[fruits[p]] == 0:
                     del count[fruits[p]]
-                
+
                 p += 1
 
-            max_fruits = max(max_fruits, s - p + 1)
+            max_fruits = max(
+                max_fruits,
+                s - p + 1
+            )
 
         return max_fruits

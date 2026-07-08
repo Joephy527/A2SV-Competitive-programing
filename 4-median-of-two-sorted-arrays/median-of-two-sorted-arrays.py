6807aea9c9da27537a540b81f110ed25edf1a275
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-08 15:09:41 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (41.48%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,28 +1,56 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
-
         n, m = len(nums1), len(nums2)
-        left, right = 0, n
+
+        if n > m:
+            nums1, nums2 = nums2, nums1
+            n, m = m, n
+
+        total_length = n + m
+        half = (total_length + 1) // 2
+
+        left = 0
+        right = n
 
         while left <= right:
-            i = (left + right) // 2
-            j = ((n + m + 1) // 2) - i
+            partition_one = (left + right) // 2
+            partition_two = half - partition_one
 
-            a_left = nums1[i - 1] if i > 0 else float("-inf")
-            a_right = nums1[i] if i < n else float("inf")
-            b_left = nums2[j - 1] if j > 0 else float("-inf")
-            b_right = nums2[j] if j < m else float("inf")
+            left_one_max = (
+                float("-inf")
+                if partition_one == 0
+                else nums1[partition_one - 1]
+            )
+            right_one_min = (
+                float("inf")
+                if partition_one == n
+                else nums1[partition_one]
+            )
 
-            if a_left <= b_right and b_left <= a_right:
-                l, r = max(a_left, b_left), min(a_right, b_right)
-                if (m + n) % 2:
-                    return float(l)
-                
-                return (l + r) / 2
+            left_two_max = (
+                float("-inf")
+                if partition_two == 0
+                else nums2[partition_two - 1]
+            )
+            right_two_min = (
+                float("inf")
+                if partition_two == m
+                else nums2[partition_two]
+            )
 
-            if a_left > b_right:
-                right = i - 1
+            if (
+                left_one_max <= right_two_min
+                and left_two_max <= right_one_min
+            ):
+                left_max = max(left_one_max, left_two_max)
+                right_min = min(right_one_min, right_two_min)
+
+                if total_length % 2 == 1:
+                    return float(left_max)
+
+                return (left_max + right_min) / 2
+
+            if left_one_max > right_two_min:
+                right = partition_one - 1
             else:
-                left = i + 1
+                left = partition_one + 1

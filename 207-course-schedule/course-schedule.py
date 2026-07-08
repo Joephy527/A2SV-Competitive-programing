39bf59c1857374aedebf34f3961044e2fea148a2
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-08 17:24:43 +0300

    Time: 4 ms (65.02%) | Memory: 20.3 MB (78.95%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        visited = [0] * numCourses
-        indegree = [0] * numCourses
+        dependencies = [[] for _ in range(numCourses)]
         queue = deque()
+        indegree = [0] * numCourses
 
         for a, b in prerequisites:
-            graph[b].append(a)
+            dependencies[b].append(a)
             indegree[a] += 1
 
-        for idx, num in enumerate(indegree):
-            if not num:
-                queue.append(idx)
+        for course in range(numCourses):
+            if not indegree[course]:
+                queue.append(course)
 
         while queue:
             course = queue.popleft()
-            visited[course] = True
 
-            for c in graph[course]:
-                indegree[c] -= 1
-
-                if not indegree[c] and not visited[c]:
-                    queue.append(c)
+            for dependency in dependencies[course]:
+                indegree[dependency] -= 1
+                if not indegree[dependency]:
+                    queue.append(dependency)
 
         return not sum(indegree)

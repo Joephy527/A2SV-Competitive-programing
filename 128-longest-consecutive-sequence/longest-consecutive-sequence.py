d31928ad058b96faf252136981a790b0ab61cb6b
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-12 16:26:24 +0300

    Time: 320 ms (5.02%) | Memory: 62.1 MB (5.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,15 +1,37 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        seen = set(nums)
-        max_len = 0
+        if not nums:
+            return 0
 
-        for num in seen:
-            if num - 1 not in seen:
-                length = 0
-                
-                while num + length in seen:
-                    length += 1
+        parents = {num: num for num in nums}
+        heights = {num: 1 for num in nums}
 
-                max_len = max(max_len, length)
+        def find(num):
+            if num not in parents:
+                return float("inf")
 
-        return max_len
+            if parents[num] != num:
+                parents[num] = find(parents[num])
+
+            return parents[num]
+
+        def union(num):
+            root_num, root_num1 = find(num), find(num - 1)
+
+            if (
+                float("inf") in (root_num, root_num1) or
+                root_num == root_num1
+            ):
+                return
+
+            if heights[root_num] <= heights[root_num1]:
+                heights[root_num1] += heights[root_num]
+                parents[root_num] = root_num1
+            else:
+                heights[root_num] += heights[root_num1]
+                parents[root_num1] = root_num
+
+        for num in nums:
+            union(num)
+
+        return max(heights.values())

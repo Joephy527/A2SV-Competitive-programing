e81e30027b2e898ba13eb3248de55b5274339c30
Yoseph Ephrem Kifle <joephy527@gmail.com>
2026-07-22 00:04:28 +0300

    Time: 257 ms (31.79%) | Memory: 21.8 MB (36.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         islands = 0
 
-        def is_inbound(row, col):
+        def is_inbounds(row, col):
             return rows > row >= 0 <= col < cols
 
         def is_land(row, col):
             return grid[row][col] == "1"
 
-        def bfs(start_row, start_col):
-            queue = deque([(start_row, start_col)])
-            grid[start_row][start_col] = "0"
+        def dfs(row, col):
+            grid[row][col] = "0"
 
-            while queue:
-                row, col = queue.popleft()
+            for x, y in directions:
+                r, c = row + x, col + y
 
-                for x, y in directions:
-                    r, c = row + x, col + y
-
-                    if is_inbound(r, c) and is_land(r, c):
-                        grid[r][c] = "0"
-                        queue.append((r, c))
+                if is_inbounds(r, c) and is_land(r, c):
+                    dfs(r, c)
 
         for row in range(rows):
             for col in range(cols):
                 if is_land(row, col):
-                    bfs(row, col)
                     islands += 1
+                    dfs(row, col)
 
         return islands
